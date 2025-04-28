ba1e481ee067db71cf546f2069eb110811c77890
Eric Nordstrom <eanord4@gmail.com>
2025-04-28 14:22:29 -0500

    Add `tools` folder with `asset` subfolder and `get_originals` package

---
+++ tools/asset/get_originals/__main__.py
@@ -0,0 +1,4 @@
+from .core import get_all
+
+if __name__ == "__main__":
+	get_all()
---
+++ tools/asset/get_originals/core.py
@@ -0,0 +1,61 @@
+"""Tools for obtaining original unmodified assets"""
+
+
+#==== SETUP ====#
+
+from chess import Board, svg
+from from_root import from_root
+import os
+
+
+
+
+#==== DATA ====#
+
+originals_relpath = os.path.join(
+	"assets",
+	"originals",
+)
+
+originals_path = from_root(originals_relpath)
+
+
+
+
+#==== FUNCTIONS ====#
+
+def std_board_svg(size: int) -> str:
+	"""Make a standard starting chess board SVG from chess.com's `chess` package"""
+
+	return svg.board(Board(), size=size)
+
+
+def get_all() -> None:
+	"""Obtain all original assets"""
+
+	print("Getting original assets...")
+
+	# standard board SVG
+	filename = "std_board.svg"
+	print(f"\n\tObtaining {filename}...")
+	svg_str = std_board_svg(350)
+	path = os.path.join(originals_path, filename)
+	with open(path, "w") as f:
+		f.write(svg_str)
+	repr_path = os.path.join("", originals_relpath, filename)
+	print(f"\t\tSaved to '{repr_path}'")
+
+	print("\nDone")
+
+
+
+
+#==== USAGE ====#
+
+__all__ = [
+
+	# functions
+	"std_board_svg",
+	"get_all",
+
+]
