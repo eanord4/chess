05dc4bc4b5ccb7522004b2233b1d9e42db5a0fed
Eric Nordstrom <eanord4@gmail.com>
2025-04-28 18:11:44 -0500

    In `svg/extract/core.py`, import entire  module and modify annotations accordingly

--- tools/asset/svg/extract/core.py
+++ tools/asset/svg/extract/core.py
@@ -3,7 +3,7 @@
 
 #==== SETUP ====#
 
-from bs4 import BeautifulSoup
+import bs4
 from from_root import from_root
 import os
 
@@ -29,13 +29,13 @@ extracted_path = from_root(extracted_relpath)
 
 #==== FUNCTIONS ====#
 
-def load_original(filename="std_board.svg") -> BeautifulSoup:
+def load_original(filename="std_board.svg") -> bs4.BeautifulSoup:
 	"""Load an original SVG as an XML-parsed BeautifulSoup"""
 
 	path = os.path.join(originals_path, filename)
 	with open(path, "r") as f:
 		contents = f.read()
-	return BeautifulSoup(contents, "lxml")
+	return bs4.BeautifulSoup(contents, "lxml")
 
 
 def extract_all() -> None:
